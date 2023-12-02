f510f37fbba87dd0eddf2b4e72ceb28f6f9a6d3e
Rafael Porto <81491964+ThorDCowo@users.noreply.github.com>
2023-12-01 23:34:59 -0300

    Menu fix
    menu fix

--- HUDVida.gd
+++ HUDVida.gd
@@ -1,12 +1,9 @@
-extends Node2D
+extends TextureRect
 
-@export var max_coin = 5
-
-var coins = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	size.x = 18 * Global.lives
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,6 +11,5 @@ func _process(delta):
 	pass
 
 
-func _on_coin_up():
-	coins += 1
-	$CoinUp.play()
+func _on_player_hit():
+	size.x = 18 * Global.lives
---
+++ StartButton.gd
@@ -0,0 +1,5 @@
+extends Button
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://Wold0.tscn")
